1f744b0b1d79e65adda904bb1b8bb459c9ef2ce9
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-15 17:18:31 +0300

    added the supplier migration and model

---
+++ database/migrations/2023_05_15_141521_create_suppliers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone');
+            $table->string('location');
+
+            $table->unsignedBigInteger('supermarket_id')->nullable();
+            $table->foreign('supermarket_id')->references('id')->on('supermarkets')->onDelete('set null');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('suppliers');
+    }
+};
