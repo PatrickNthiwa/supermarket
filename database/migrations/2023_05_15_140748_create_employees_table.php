297d359f0df515211b5266fbfaa2f0431460b493
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-15 17:14:13 +0300

    added the employees migration

---
+++ database/migrations/2023_05_15_140748_create_employees_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('type');
+            $table->enum('gender',['male','female','custom','other'])->default('other');
+
+            $table ->unsignedBigInteger('manager_id')->nullable();
+
+            $table->foreign('manager_id')->references('id')->on('managers')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
